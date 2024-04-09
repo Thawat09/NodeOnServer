02a77942c40fded83735aa0b1517816680395d7f
Thawat <thawat@dtc.co.th>
2024-04-09 10:28:30 +0700

    test run node

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 6672;
+const port = process.env.PORT || process.argv[2] || 6672;
 
-// สร้าง API endpoint ที่ส่งคำว่า "Hello, World!" กลับมา
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-// เริ่มต้น Express server ที่ port 6672
-app.listen(6672, 'http://159.138.235.143/', () => {
-    console.log(`Server is running on http://159.138.235.143:6672`);
-  });
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
