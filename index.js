4ba76e2633ddfe13f1489ac742e65382d0e21b33
Thawat <thawat@dtc.co.th>
2024-04-09 16:26:10 +0700

    test

--- index.js
+++ index.js
@@ -9,21 +9,23 @@ const limiter = rateLimit({
 });
 
 const setHeaders = (req, res, next) => {
-  res.setHeader(
-    'Content-Security-Policy',
-    "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';"
-  );
-  res.setHeader(
-    'Strict-Transport-Security',
-    'max-age=31536000; includeSubDomains'
-  );
-  res.setHeader('X-Frame-Options', 'DENY');
-  res.setHeader('X-Content-Type-Options', 'nosniff');
-  res.setHeader('Referrer-Policy', 'no-referrer');
-  res.setHeader(
-    'Permissions-Policy',
-    'geolocation=(self); microphone=(self); interest-cohort=()'
-  );
+  if (req.secure) {
+    res.setHeader(
+      'Content-Security-Policy',
+      "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';"
+    );
+    res.setHeader(
+      'Strict-Transport-Security',
+      'max-age=31536000; includeSubDomains'
+    );
+    res.setHeader('X-Frame-Options', 'DENY');
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('Referrer-Policy', 'no-referrer');
+    res.setHeader(
+      'Permissions-Policy',
+      'geolocation=(self); microphone=(self); interest-cohort=()'
+    );
+  }
   next();
 };
 
