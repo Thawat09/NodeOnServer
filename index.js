a2eb4e437b9f5fe78b860d6b5e6fb3c052a8b83c
Thawat <thawat@dtc.co.th>
2024-04-09 11:55:20 +0700

    test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || process.argv[2] || 6672;
+const port = 6672;
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
