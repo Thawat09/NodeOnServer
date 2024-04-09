a9da60a6cdd729840308d99ac08a28ae1281be42
Thawat <thawat@dtc.co.th>
2024-04-09 16:02:12 +0700

    test core

--- index.js
+++ index.js
@@ -8,7 +8,23 @@ const limiter = rateLimit({
   max: 5,
 });
 
+const setHeaders = (req, res, next) => {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';"
+  );
+  res.setHeader('X-Frame-Options', 'DENY');
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('Referrer-Policy', 'no-referrer');
+  res.setHeader(
+    'Permissions-Policy',
+    'geolocation=(self); microphone=(self); interest-cohort=()'
+  );
+  next();
+};
+
 app.use(limiter);
+app.use(setHeaders);
 app.use(indexAPI);
 
 app.listen(3000, () => {
