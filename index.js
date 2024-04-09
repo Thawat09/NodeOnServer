54e3988c14c928338afa6a0a5ae778339b0cc9bb
Thawat <thawat@dtc.co.th>
2024-04-09 15:24:08 +0700

    delete function name

--- index.js
+++ index.js
@@ -14,17 +14,3 @@ app.use(indexAPI);
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
-
-function name(params1, params2, params3, params4) {
-    return params1 +
-    params2 +
-    params3 +
-    params1;
-}
-
-name(
-    'wedf',
-    'qwfef',
-    'wefw',
-    '4frtrg'
-)
