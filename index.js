39662499d27f9747062491f8e54b1ea2da74853e
Thawat <thawat@dtc.co.th>
2024-04-09 15:42:16 +0700

    test husky

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const rateLimit = require("express-rate-limit");
+const rateLimit = require('express-rate-limit');
 const app = express();
 const indexAPI = require('./src/routes/index.js');
 
 const limiter = rateLimit({
-    windowMs: 3 * 60 * 1000,
-    max: 5
+  windowMs: 3 * 60 * 1000,
+  max: 5,
 });
 
 app.use(limiter);
 app.use(indexAPI);
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000');
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,10 @@ const aboutRoutes = require('./about');
 
 const app = express();
 
-app.use('/', (req, res, next) => req.path === '/' ? res.send('Hello User') : next());
+app.use('/', (req, res, next) =>
+  req.path === '/' ? res.send('Hello User') : next()
+);
+
 app.use('/about', aboutRoutes);
 
 module.exports = app;
