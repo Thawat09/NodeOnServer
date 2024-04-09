c29a795b50b7f4142a84342822964040d18bd80b
Thawat <thawat@dtc.co.th>
2024-04-09 14:47:29 +0700

    test nodemon

--- src/routes/about/index.js
+++ src/routes/about/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('About Us'));
+router.get('/', (req, res) => res.send('API About'));
 
 module.exports = router;
