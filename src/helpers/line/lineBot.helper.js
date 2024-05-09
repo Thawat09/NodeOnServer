314b6ab614302355a9e98dc85d57de3226c1ad01
Thawat <thawat@dtc.co.th>
2024-05-09 11:55:56 +0700

    เพิ่มภาษา

--- src/helpers/line/lineBot.helper.js
+++ src/helpers/line/lineBot.helper.js
@@ -12,6 +12,15 @@ const createNlpManager = locale => {
 const supportedLanguages = {
   en: createNlpManager('en'),
   th: createNlpManager('th'),
+  zh: createNlpManager('zh'),
+  hi: createNlpManager('hi'),
+  es: createNlpManager('es'),
+  fr: createNlpManager('fr'),
+  ru: createNlpManager('ru'),
+  ja: createNlpManager('ja'),
+  vi: createNlpManager('vi'),
+  sw: createNlpManager('sw'),
+  pt: createNlpManager('pt'),
 };
 
 const addTrainingData = (locale, texts, category) => {
@@ -46,6 +55,24 @@ addTrainingData('en', ['hello', 'hi', 'how are you'], 'greetings');
 addAnswer('en', 'greetings', ['Hello!!!']);
 addTrainingData('th', ['สวัสดี', 'หวัดดี'], 'greetings');
 addAnswer('th', 'greetings', ['สวัสดี!!!']);
+addTrainingData('zh', ['你好', '您好'], 'greetings');
+addAnswer('zh', 'greetings', ['你好!!!']);
+addTrainingData('hi', ['नमस्ते', 'हैलो'], 'greetings');
+addAnswer('hi', 'greetings', ['नमस्ते!!!']);
+addTrainingData('es', ['hola', '¿cómo estás?'], 'greetings');
+addAnswer('es', 'greetings', ['¡Hola!!!']);
+addTrainingData('fr', ['bonjour', 'salut'], 'greetings');
+addAnswer('fr', 'greetings', ['Bonjour!!!']);
+addTrainingData('ru', ['привет', 'здравствуйте'], 'greetings');
+addAnswer('ru', 'greetings', ['Привет!!!']);
+addTrainingData('ja', ['こんにちは', 'やあ'], 'greetings');
+addAnswer('ja', 'greetings', ['こんにちは!!!']);
+addTrainingData('vi', ['xin chào', 'chào bạn'], 'greetings');
+addAnswer('vi', 'greetings', ['Xin chào!!!']);
+addTrainingData('sw', ['hej', 'hallå', 'Hur mår du'], 'hälsningar');
+addAnswer('sw', 'hälsningar', ['Hej!!!']);
+addTrainingData('pt', ['olá', 'como está você'], 'saudações');
+addAnswer('pt', 'saudações', ['Olá!!!']);
 
 (async () => {
   for (const locale in supportedLanguages) {
